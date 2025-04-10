c8eff924955d4fe8708d2f8482e55473eec5d5c6
Simon Tronelle <stronelle@gmail.com>
2025-04-10 10:14:35 +0200

    Creating Dialogue and DialogueManager script

--- Assets/Game/Scripts/Pokeclope/DialogueManager.cs
+++ Assets/Game/Scripts/Pokeclope/DialogueManager.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
-
+using System.Collections;
 public class DialogueManager : MonoBehaviour 
 {
-    
+    public Queue sentences;
+    private void Start()
+    {
+        sentences = new Queue();   
+    }
 }
