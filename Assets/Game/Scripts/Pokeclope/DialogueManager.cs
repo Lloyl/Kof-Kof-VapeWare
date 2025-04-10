c09bca3ef2c6d5a8aa551dd2c5b4391efcb631e9
Simon Tronelle <stronelle@gmail.com>
2025-04-10 10:31:58 +0200

    Implemented DialogueManager

--- Assets/Game/Scripts/Pokeclope/DialogueManager.cs
+++ Assets/Game/Scripts/Pokeclope/DialogueManager.cs
@@ -1,10 +1,41 @@
 using UnityEngine;
-using System.Collections;
+using System;
+using System.Collections.Generic;
 public class DialogueManager : MonoBehaviour 
 {
     public Queue sentences;
     private void Start()
     {
-        sentences = new Queue();   
+        sentences = new Queue<string>();   
+    }
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log("Start of dialogue");
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of dialogue");
     }
 }
